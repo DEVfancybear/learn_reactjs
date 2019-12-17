8f852cffdd3a8946604ecd0ee2d3e2d7c38ad331
DEVfancybear <duonghacker2345@gmail.com>
2019-12-17 11:41:13 +0700

    hoan thanh React.createRef()

---
+++ example_4/src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.inputElement = React.createRef();
+  }
+  componentDidMount() {
+    this.inputElement.current.focus();
+  }
+  render () {
+    return (
+      <>
+      <div className="App">
+      <input type="text" ref={this.inputElement}/>
+      </div>
+      </>
+    )
+  }
+}
+
+export default App;
